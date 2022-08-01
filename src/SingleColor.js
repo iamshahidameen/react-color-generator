72209c598a0b67ab7faa467b73c4255cf8b19f52
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-01 20:40:17 +0500

    Converted RGB colour into hex colour through StackOverflow function, also changes text colour of the boxes which have darker shades

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,19 +2,26 @@ import React, { useState, useEffect } from 'react';
 import rgbToHex from './utils';
 
 const SingleColor = ({ rgb, weight, index }) => {
-  let colorValue = [];
+  const bgColor = rgb.join(',');
 
-  colorValue = [...rgb];
-  console.log(colorValue);
+  {
+    /* RGB to Hex Color conversion through StackOverflow function method  */
+  }
 
+  const hex = rgbToHex(...rgb);
   return (
     <>
       <article
-        className="color false"
-        style={{ backgroundColor: `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})` }}
+        className={`color ${index > 10 ? 'color-light' : 'color-value'}`}
+        //  My method for inline color
+        // style={{ backgroundColor: `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})` }}
+
+        //  Alternate Method for inline-color
+        style={{ backgroundColor: `rgb(${bgColor})` }}
       >
-        <p className="percent-value">100%</p>
-        <p className="color-value">#ffffff</p>
+        <p className="percent-value">{weight}%</p>
+
+        <p className="color-value">{hex}</p>
       </article>
     </>
   );
